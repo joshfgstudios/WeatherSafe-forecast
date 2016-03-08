cdebb4393cdaa74d8c501cf3451f2b28c9efb51f
Joshua Ide <Josh@192-168-1-100.tpgi.com.au>
2016-03-08 11:35:59 +1100

    Initial Commit

---
+++ WeatherSafe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherSafe
+//
+//  Created by Joshua Ide on 8/03/2016.
+//  Copyright © 2016 Fox Gallery Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
