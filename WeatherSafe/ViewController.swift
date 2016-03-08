4971da04a0dc408ff64c7f504e249de346bc489a
Joshua Ide <Josh@192-168-1-100.tpgi.com.au>
2016-03-08 13:09:49 +1100

    reverse geocoding location to print city

--- WeatherSafe/ViewController.swift
+++ WeatherSafe/ViewController.swift
@@ -7,17 +7,77 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    //Outlets
+    //------------
+    @IBOutlet weak var lblCityName: UILabel!
+    
+    //Properties
+    //------------
+    let locationManager = CLLocationManager()
+    let geoCoder = CLGeocoder()
+    var currentLocation: CLLocation?
+    var cityName: String?
+    var countryName: String?
+    
+    
+    //Functions
+    //------------
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self
+        locationAuthStatus()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        if CLLocationManager.authorizationStatus() != .AuthorizedWhenInUse {
+            let alert = UIAlertController(title: "Unable to access location", message: "We don't have permission to access your location, and so we can't provide you with local weather data.  Please enable us to use your location via Settings to get weather information.", preferredStyle: UIAlertControllerStyle.Alert)
+            let settingsAction = UIAlertAction(title: "Settings", style: .Default, handler: { (UIAlertAction) -> Void in
+                let settingsURL = NSURL(string: "prefs:root=LOCATION_SERVICES")
+                if let url = settingsURL {
+                    UIApplication.sharedApplication().openURL(url)
+                }
+            })
+            let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+            alert.addAction(settingsAction)
+            alert.addAction(cancelAction)
+            
+            presentViewController(alert, animated: true, completion: nil)
+        } else {
+            dismissViewControllerAnimated(true, completion: nil)
+            updateUI()
+        }
+    }
+    
+    func locationAuthStatus() {
+        if CLLocationManager.authorizationStatus() != .AuthorizedWhenInUse {
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    func updateUI() {
+        getCityName()
+    }
+    
+    func getCityName() {
+        currentLocation = locationManager.location
+        geoCoder.reverseGeocodeLocation(currentLocation!) { (placemarks, error) -> Void in
+            var placeMark: CLPlacemark?
+            placeMark = placemarks?[0]
+            
+            if let cityName = placeMark?.addressDictionary!["City"] as? String {
+                self.cityName = cityName
+                if let countryName = placeMark?.addressDictionary!["CountryCode"] as? String {
+                    self.countryName = countryName
+                    self.lblCityName.text = "\(self.cityName!), \(self.countryName!)"
+                }
+            }
+            
+        }
     }
 
 
